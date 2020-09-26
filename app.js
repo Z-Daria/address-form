53380ebbb4a28a86a20eb9f523adb6b10c6d1928
Daria <daria.zakharova1404@gmail.com>
2020-09-26 17:11:55 +0300

    Adding functions to request address hints

---
+++ app.js
@@ -0,0 +1,62 @@
+let url = 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address';
+let token = '1c332134a31a6b7c61298afafc21083d47d5d5d0';
+let choices;
+
+$(document).ready(() => {
+    // to request hints while entering address
+    $('#address').on('keyup focus change', function(event) {
+        $('datalist').remove();
+        let container = event.target;
+        getSuggestions(container);
+    })
+});
+
+// to fetch address hints from API
+function getSuggestions(container) {
+    const request = $.ajax({
+        url: url,
+        type: 'POST',
+        data: JSON.stringify({query: $('#address').val()}),
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Token " + token
+        }
+    });
+    request.done(function(data) {
+        createOptionsList(container, data.suggestions);
+    });
+    request.fail(err => {
+        console.log(err);
+    });
+};
+
+// to create datalist for suggestions
+function createOptionsList(container, suggestions) {
+    choices = suggestions;
+    let options = '<datalist id="' + container.attributes[1].value + '">';
+    for (let i = 0; i < suggestions.length; i++) {
+        options += '<option value="' + suggestions[i].value + '">';
+    };
+    options += '</datalist>';
+    $(options).insertAfter(container);
+};
+
+$('#address').on('blur', event => {
+    if (choices) {
+        $('#address').unbind('keyup focus change');
+        let selected = event.target.value;
+        fillinFields(choices.find(x => x.value == selected));
+    }
+});
+
+// to fill in the form fields when the suggestion is selected
+function fillinFields(selected) {
+    $('#region').val(selected.data.region);
+    $('#district').val(selected.data.area_with_type);
+    $('#city').val(selected.data.city);
+    $('#settlement').val(selected.data.settlement_with_type);
+    $('#street').val(selected.data.street);
+    $('#house').val(selected.data.house);
+};
+
